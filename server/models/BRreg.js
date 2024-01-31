775849f5fc32ca02522ee0473f92f10d0fc1b75b
Mark Daniel Edillor <edillormark2@gmail.com>
2024-01-31 08:02:17 +0800

    Adding registration models

--- server/models/BRreg.js
+++ server/models/BRreg.js
@@ -16,3 +16,21 @@ const BRregSchema = new mongoose.Schema({
 const BRreg = mongoose.model("BRreg", BRregSchema);
 
 export default BRreg;
+const mongoose = require("mongoose");
+
+const brregSchema = new mongoose.Schema({
+  email: String,
+  qualification: String,
+  surname: String,
+  firstname: String,
+  locality: String,
+  status: String,
+  grade: String,
+  attended: String,
+  school: String,
+  contact: Number,
+  amen: String
+});
+
+const BRregModel = mongoose.model("BRreg", brregSchema);
+module.exports = BRregModel;
